8f033e675850cb3bd79470a5eaf154bd9e0e3b75
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-03-08 15:56:32 -0700

    update

--- MovieReview.cpp
+++ MovieReview.cpp
@@ -9,14 +9,28 @@ struct Node{
     Node *next;
 };
 
-void addNodeHEAD(Node *, Node *);
-void addNodeTAIL(Node *, Node*);
+void addNode(Node *, Node *, int);
 void output(Node*);
 
 
 int main(){
     Node *head = nullptr;
+
+    int choice;
+
+    cout << "Which linked list method should we use?\n";
+    cout << "\t[1] New nodes are added to the head of the list\n";
+    cout << "\t[2] New nodes are added to the tail of the list\n";
+    cout << "\tChoice: ";
+    cin >> choice;
+
     
 
     return 0;
+}
+
+void addNode(Node *head, Node *current, int choice){
+    if (head -> next = nullptr){
+        head -> next = current;
+    }
 }
